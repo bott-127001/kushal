3ac3a2cd29608b2d580e96e3f465347cddb48edc
bott-127001 <samsalgar44@gmail.com>
2025-06-29 14:37:12 +0530

    Update HomePage.js

--- client/src/components/homepage/HomePage.js
+++ client/src/components/homepage/HomePage.js
@@ -9,12 +9,12 @@ import Footer from './Footer'
 
 function HomePage () {
   return (
-    <div className='bg-gray-50 min-h-screen flex flex-col mb-1'>
+    <div className='bg-gray-50 min-h-screen flex flex-col'>
       <div className='bg-gray-100'><HeroSection /></div>
-      <div className='bg-gray-200 mb-1'><ZodiacFinder /></div>
-      <div className='bg-gray-100 mb-1'><FeaturedGemstones /></div>
-      <div className='bg-gray-200 mb-1'><AstrologicalGuidance /></div>
-      <div className='bg-gray-100 mb-1'><CelestialInsights /></div>
+      <div className='bg-gray-200'><ZodiacFinder /></div>
+      <div className='bg-gray-100'><FeaturedGemstones /></div>
+      <div className='bg-gray-200'><AstrologicalGuidance /></div>
+      <div className='bg-gray-100'><CelestialInsights /></div>
       <div className='bg-gray-200'><Newsletter /></div>
       <Footer />
     </div>
